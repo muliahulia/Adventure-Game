050d330dc0f699f5012440002f28093577ad5c81
darronese <darron198@hotmail.com>
2024-04-14 15:47:30 -0700

    updated main.cpp
    commented out the original windows, added main menu to test

--- PA_9/PA_9/main.cpp
+++ PA_9/PA_9/main.cpp
@@ -1,25 +1,32 @@
 #include <SFML/Graphics.hpp>
 #include <SFML/Audio.hpp>
+#include <SFML/Graphics/RenderWindow.hpp>
+
+#include "menu.hpp"
 
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
-    sf::CircleShape shape(100.f);
-    shape.setFillColor(sf::Color::Green);
+    /*
+      sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
+      sf::CircleShape shape(100.f);
+      shape.setFillColor(sf::Color::Green);
 
-    while (window.isOpen())
-    {
-        sf::Event event;
-        while (window.pollEvent(event))
-        {
-            if (event.type == sf::Event::Closed)
-                window.close();
-        }
+      while (window.isOpen())
+      {
+          sf::Event event;
+          while (window.pollEvent(event))
+          {
+              if (event.type == sf::Event::Closed)
+                  window.close();
+          }
 
-        window.clear();
-        window.draw(shape);
-        window.display();
-    }
-
+          window.clear();
+          window.draw(shape);
+          window.display();
+      }
+    */
+    //Main menu test
+    Menu mainMenu;
+    mainMenu.menuWrapper();
     return 0;
 }
